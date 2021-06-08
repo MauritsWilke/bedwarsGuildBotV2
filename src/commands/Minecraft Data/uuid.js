a462227616300bbcb865d4447a34c91a2c1d7501
Maurits Wilke <mauritswilke@gmail.com>
2021-06-08 15:40:09 +0200

    fixed UUID return statement (dumbass)

--- src/commands/Minecraft Data/uuid.js
+++ src/commands/Minecraft Data/uuid.js
@@ -16,37 +16,28 @@ module.exports = {
     canTakeIGN: true,
 
     async run (client, message, args, Discord){
-        try{
-            const playerUUID = await getUUID(args[0]).catch(e=>{
-                const errorEmbed = new Discord.MessageEmbed()
-                  .setColor(config.colours.error)
-                  .setDescription(`Error: ${betterText(e)}\nType ${config.prefix}help uuid to see more info`)
-                  .setThumbnail(config.images.error)
-                  .setTimestamp()
-                  .setFooter(config.name, client.user.displayAvatarURL());
-                return message.channel.send(errorEmbed)
-            })
+      try{
+        const playerUUID = await getUUID(args[0])
+      }catch(e){
+          const errorEmbed = new Discord.MessageEmbed()
+            .setColor(config.colours.error)
+            .setDescription(`Error: ${betterText(e)}\nType ${config.prefix}help uuid to see more info`)
+            .setThumbnail(config.images.error)
+            .setTimestamp()
+            .setFooter(config.name, client.user.displayAvatarURL());
+          return message.channel.send(errorEmbed);
+      }
 
-            const uuidEmbed = new Discord.MessageEmbed()
-              .setColor(config.colours.default)
-              .setTitle('UUID Converting')
-              .addFields(
-                { name: `**Username**`, value: `${betterText(args[0])}`, inline: false },
-                { name: `**UUID**`, value: `${betterText(`${playerUUID}`)}`, inline: false }
-              ) 
-              .setThumbnail(`https://minotar.net/helm/${playerUUID}.png`)
-              .setTimestamp()
-              .setFooter(config.name, client.user.displayAvatarURL());
-            return message.channel.send(uuidEmbed)
-
-        }catch(e){
-            const errorEmbed = new Discord.MessageEmbed()
-                  .setColor(config.colours.error)
-                  .setDescription(`Error: ${betterText(e)}\nType ${config.prefix}help uuid to see more info`)
-                  .setThumbnail(config.images.error)
-                  .setTimestamp()
-                  .setFooter(config.name, client.user.displayAvatarURL());
-            return message.channel.send(errorEmbed)
-        }
+        const uuidEmbed = new Discord.MessageEmbed()
+          .setColor(config.colours.default)
+          .setTitle('UUID Converting')
+          .addFields(
+            { name: `**Username**`, value: `${betterText(args[0])}`, inline: false },
+            { name: `**UUID**`, value: `${betterText(`${playerUUID}`)}`, inline: false }
+          ) 
+          .setThumbnail(`https://minotar.net/helm/${playerUUID}.png`)
+          .setTimestamp()
+          .setFooter(config.name, client.user.displayAvatarURL());
+        return message.channel.send(uuidEmbed)
     }
 }
